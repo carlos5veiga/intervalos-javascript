c43c210137ba598ceeb2f9a31f98bb409c9887f3
Carlos Veiga Filho <carlos5veiga@gmail.com>
2023-08-04 12:27:40 -0300

    ta perto

--- js/segundas.js
+++ js/segundas.js
@@ -4,6 +4,7 @@ let erros = 0;
 function acerto() {
     acertos++;
     document.getElementById("acertos").innerText = acertos;
+    document.getElementById("nota1").play();
 }
 
 function erro() {
@@ -18,12 +19,31 @@ function randomNumber(min, max) {
 }
 
 function play() {
-    let fundamental = randomNumber(1, 9);
+    let fundamental = randomNumber(5, 20);
     console.log("fundamental: " + fundamental);
     let sentido = randomNumber(1, 2);
     console.log("sentido: " + sentido);
     let natureza = randomNumber(1, 2);
     console.log("natureza: " + natureza);
+
+    let nota1 = document.getElementById("nota1");
+    nota1.src = "./notas/" + fundamental + ".mp3";
+    nota1.load();
+
+    let intervalo = fundamental;
+
+    if (sentido === 1) {
+        intervalo -= natureza;
+    } else {
+        intervalo += natureza;
+    }
+
+    let nota2 = document.getElementById("nota2");
+    nota2.src = "./notas/" + intervalo + ".mp3";
+    nota2.load();
+
+    document.getElementById("nota1").play();
+    setTimeout(() => {document.getElementById("nota2").play();}, 1500)
 }
 
 
